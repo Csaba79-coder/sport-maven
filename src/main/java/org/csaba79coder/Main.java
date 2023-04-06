d1b9a4e42ba07c33a091591e0df530b3a3ca2163
Csaba79-coder <csabavadasz79@gmail.com>
2023-04-06 21:26:57 +0200

    refactor main

--- src/main/java/org/csaba79coder/Main.java
+++ src/main/java/org/csaba79coder/Main.java
@@ -8,12 +8,17 @@ import java.net.URISyntaxException;
 public class Main {
 
     public static void main(String[] args) {
-        FootballController footballController = new FootballController();
+        try {
+            System.out.println(new FootballController().renderAllFootballTeams());
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+        /*FootballController footballController = new FootballController();
         try {
             Object result = footballController.renderAllFootballTeams();
             System.out.println(result);
         } catch (URISyntaxException | IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }
